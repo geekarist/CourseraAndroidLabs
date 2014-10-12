152a177a454e8aa779850ba77f3cf053489868c0
chrp <christophe.pele@gmail.com>
2014-10-12 14:42:20 +0200

    Complete intents lab

--- intentsLab/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
+++ intentsLab/src/main/java/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -46,13 +46,17 @@ public class ExplicitlyLoadedActivity extends Activity {
 
 		Log.i(TAG,"Entered enterClicked()");
 		
-		// TODO - Save user provided input from the EditText field
+		// Save user provided input from the EditText field
+        String userEnteredText = mEditText.getText().toString();
 
-		// TODO - Create a new intent and save the input from the EditText field as an extra
-		
-		// TODO - Set Activity's result with result code RESULT_OK
-		
-		// TODO - Finish the Activity
+        // Create a new intent and save the input from the EditText field as an extra
+        Intent intent = new Intent();
+        intent.putExtra("user_entered_text", userEnteredText);
 
+        // Set Activity's result with result code RESULT_OK
+        setResult(RESULT_OK, intent);
+
+		// Finish the Activity
+        finish();
 	}
 }
