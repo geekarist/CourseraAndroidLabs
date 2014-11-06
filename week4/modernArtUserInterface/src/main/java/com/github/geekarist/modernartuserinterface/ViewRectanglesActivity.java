0ac785241c1802c59db65abcbb05473d3442666d
Christophe Pelé <christophe.pele.prestataire@sfr.com>
2014-11-06 23:19:11 +0100

    Change colors when user drags seekbar

--- week4/modernArtUserInterface/src/main/java/com/github/geekarist/modernartuserinterface/ViewRectanglesActivity.java
+++ week4/modernArtUserInterface/src/main/java/com/github/geekarist/modernartuserinterface/ViewRectanglesActivity.java
@@ -1,9 +1,11 @@
 package com.github.geekarist.modernartuserinterface;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 
@@ -13,6 +15,30 @@ public class ViewRectanglesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_rectangles);
+
+        SeekBar changeColors = (SeekBar) findViewById(R.id.change_colors);
+        final int leftTopRectOriginalColor = ((ColorDrawable) findViewById(R.id.soft_rectangle_left_top).getBackground()).getColor();
+        final int leftBottomRectOriginalColor = ((ColorDrawable) findViewById(R.id.soft_rectangle_left_bottom).getBackground()).getColor();
+        final int rightTopRectOriginalColor = ((ColorDrawable) findViewById(R.id.heavy_rectangle_right_top).getBackground()).getColor();
+        final int rightBottomRectOriginalColor = ((ColorDrawable) findViewById(R.id.heavy_rectangle_right_bottom).getBackground()).getColor();
+
+        changeColors.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                findViewById(R.id.soft_rectangle_left_top).setBackgroundColor(leftTopRectOriginalColor + progress);
+                findViewById(R.id.soft_rectangle_left_bottom).setBackgroundColor(leftBottomRectOriginalColor + progress);
+                findViewById(R.id.heavy_rectangle_right_top).setBackgroundColor(rightTopRectOriginalColor + progress);
+                findViewById(R.id.heavy_rectangle_right_bottom).setBackgroundColor(rightBottomRectOriginalColor + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 
 
