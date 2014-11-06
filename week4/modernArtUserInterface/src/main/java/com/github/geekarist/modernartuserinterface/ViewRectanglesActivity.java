d201846ee069fb227f52fdda808a2c4709a4feda
Christophe Pelé <christophe.pele.prestataire@sfr.com>
2014-11-06 22:16:41 +0100

    Don't use a fragment

--- week4/modernArtUserInterface/src/main/java/com/github/geekarist/modernartuserinterface/ViewRectanglesActivity.java
+++ week4/modernArtUserInterface/src/main/java/com/github/geekarist/modernartuserinterface/ViewRectanglesActivity.java
@@ -1,13 +1,9 @@
 package com.github.geekarist.modernartuserinterface;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 
@@ -17,11 +13,6 @@ public class ViewRectanglesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_rectangles);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -43,20 +34,5 @@ public class ViewRectanglesActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_view_rectangles, container, false);
-        }
     }
 }
