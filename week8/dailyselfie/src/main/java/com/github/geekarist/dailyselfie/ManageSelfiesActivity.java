47b87093f964ed735fdcb1cfc166c29355049029
geekarist <christophe.pele@gmail.com>
2014-12-23 19:57:00 +0100

    Save picture
    - In the activity:
        * Create a file for the output picture before taking the photo
        * Specify the expected output picture file in the intent
        * In case of error, display a toast and log the stack trace
        * In case of success, display a toast with the picture file path
    - In the manifest, use a permission for writing to external storage

--- week8/dailyselfie/src/main/java/com/github/geekarist/dailyselfie/ManageSelfiesActivity.java
+++ week8/dailyselfie/src/main/java/com/github/geekarist/dailyselfie/ManageSelfiesActivity.java
@@ -2,16 +2,27 @@ package com.github.geekarist.dailyselfie;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class ManageSelfiesActivity extends Activity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = ManageSelfiesActivity.class.getSimpleName();
+
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +58,41 @@ public class ManageSelfiesActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Toast.makeText(this, "Ok!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.picture_taken, mCurrentPhotoPath), Toast.LENGTH_SHORT).show();
         }
     }
 
     private void takePicture() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            try {
+                File pictureFile = createImageFile();
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(pictureFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } catch (IOException e) {
+                Toast.makeText(this, R.string.error_taking_picture, Toast.LENGTH_LONG).show();
+                Log.e(TAG, getString(R.string.error_taking_picture), e);
+            }
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+
+        if (storageDir.mkdirs() || storageDir.isDirectory()) {
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+            mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+            return image;
+        }
+
+        return null;
     }
 }
